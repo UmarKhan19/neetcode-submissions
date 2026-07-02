aabf946f7919d280361ca100cbb85be46c441a5e
Umar Khan <khanumar0108@gmail.com>
2026-07-02 23:45:43 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        const result: string[] = [];
+
+        for (const str of strs) {
+            result.push(str.length + "#" + str);
+        }
+
+        return result.join("");
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const resultArr: string[] = [];
+
+        let curr = 0;
+        let i: number = 0;
+
+        while (i < str.length) {
+            if (str[i] === "#") {
+                const numOfChar = Number(str.substring(curr, i));
+                resultArr.push(str.substring(i + 1, i + 1 + numOfChar));
+                curr = i + 1 + numOfChar;
+                i = curr;
+            }
+            i++
+        }
+
+        return resultArr;
+    }
+}
